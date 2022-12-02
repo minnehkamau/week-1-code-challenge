c4f2b40e144ea9d34771b27285ed31c96fffa180
minnehkamau <minnieches08@gmail.com>
2022-12-02 17:06:09 +0300

    salary calculator

--- salary.js
+++ salary.js
@@ -3,21 +3,41 @@ let pay=prompt("please enter your grosSalary")
 let grosSalary=pay;
 function SalaryPayable(){
 let paye;
-let nssf;
+let nssf =400;
 let nhif;
-let netSalary;
+let deductions;
 
-    if (grosSalary< 32334){
-    paye = (grosSalary*0.1);
-    console.log(paye)
+if (grosSalary<24000){
+    paye=(grosSalary*0.1)
+}
+else if(grosSalary< 32332 && grosSalary >24000){
+    paye= (grosSalary*0.25)
+}
+else if (grosSalary>32333){
+    paye=(grosSalary*0.3)
+}
+else if (grosSalary< 6000){
+        nhif=150;
     }
-    
+else if(grosSalary>=6000 && grosSalary<8000){
+    nhif=300 ;   
+}
+else if(grosSalary>=8000 && grosSalary<12000){
+    nhif= 400;
+}
+else if(grosSalary>=12000 && grosSalary<15000){
+    nhif=500;
+}
+else if( grosSalary>=15000 && grosSalary<20000){
+    nhif=600;
+}
+else if(grosSalary>=20000 && grosSalary<25000){
+        nhif=700
+    }
+if(deductions= (paye+nhif+nssf)){
 
-
-
-
-
-
-
+const netSalary=(grosSalary-deductions)
+        console.log(netSalary);
+}
 }
 SalaryPayable()
